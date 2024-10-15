fe179855ef60824eb13b72faec66b3afbd5c850b
Gaurav Ghuge <guduughuge7@gmail.com>
2024-10-15 19:50:02 +0530

    create the boiler plate code

---
+++ 01_introduction_to_java/pivot.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class pivot {
+
+   public static void main(String args[]) {
+
+      
+      // System.out.println("gaurav ghuge");
+
+      int a = 10;
+      System.out.println(a);
+      
+
+
+   }
+
+}
