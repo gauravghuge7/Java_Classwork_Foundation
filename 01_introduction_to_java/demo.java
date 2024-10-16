4b3e64d5b686349a5a2f6cee43a16c923214af5a
Gaurav Ghuge <guduughuge7@gmail.com>
2024-10-16 22:30:30 +0530

    printing in java

---
+++ 01_introduction_to_java/demo.java
@@ -0,0 +1,18 @@
+
+class demo {
+
+	
+	public static void main(String[] rutu) {
+		
+		
+		//System.out.print("this is demo ");
+		
+		
+		//System.out.print(rutu[0]);
+
+		System.out.printf("%d", 193794);
+
+	}
+
+
+}
