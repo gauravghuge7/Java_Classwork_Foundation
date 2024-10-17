7c962c23407e3edb3d93af423b83df933c80fbfd
Gaurav Ghuge <guduughuge7@gmail.com>
2024-10-17 21:10:42 +0530

    update the code

---
+++ 01_introduction_to_java/c_print/print.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+void main() {
+
+	printf(" this is the execution file ");
+
+}
---
+++ 01_introduction_to_java/gaurav.java
@@ -0,0 +1,13 @@
+
+class gaurav {
+	
+	public static void main(String arr[]) {
+
+		System.out.println("gaurav ghuge");
+		
+		System.out.print("gaurav ghuge");
+		
+	}
+
+
+}
