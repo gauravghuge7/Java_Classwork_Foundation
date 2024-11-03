d12121ec6f12040bb222f6b8ce5b7fe65eb789ca
Gaurav Ghuge <guduughuge7@gmail.com>
2024-11-03 21:58:00 +0530

    update data type

--- 02_data_types/index.java
+++ 02_data_types/index.java
@@ -9,7 +9,46 @@ public class index {
 
 	byte chaiPrice = 127;
 	
-	System.out.println(" chai price =  " + chaiPrice);	
+	// System.out.println(" chai price =  " + chaiPrice);	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	byte gaurav = 127;
+
+
+	// System.out.println(gaurav);
+
+
+	short demo = 32767;
+
+
+	int demo1 = 2147483647;
+	
+
+	// long data = 922337203685477580;
+
+
+	// System.out.println(data);
+
+
+	float a = 23423.234563645f;
+	double b = 23423.234563645f;
+
+	System.out.println(a);
 
 
    }
