0d2ccfbcece08c138a81eaa8e03014e24a05c146
Gaurav Ghuge <guduughuge7@gmail.com>
2024-11-15 22:11:17 +0530

    update revision class

---
+++ 02_data_types/temp.java
@@ -0,0 +1,43 @@
+
+
+class temp {
+
+   public static void main(String args[]) {
+
+
+      byte chai = 10;
+
+      System.out.println(chai);
+
+      short samsung = 29000;
+      System.out.println(samsung);
+
+      int lenovo_laptop = 55000;
+
+      System.out.println(lenovo_laptop);
+
+
+      long railway = 922337203;
+
+      System.out.println(railway);
+
+
+
+      boolean data = true;
+
+      System.out.println(data);
+
+      char atRate = '@';
+
+      System.out.println(atRate);
+
+
+      float stockPrice = 1234.45345345f;
+      System.out.println(stockPrice);
+
+      double moreValue = 3453.563456f;
+
+      System.out.println(moreValue);
+
+   }
+}
